70acdf41bef3b5d16a82258bd975922ab8c3bca3
kimjinu <kimjinu5555@gmail.com>
2022-02-20 17:47:40 +0900

    [Modify] Guide 관련 UserDefault R/W 인터페이스 추가 (isShowGuide, fetchGuideHistory, setGudieHistory)

--- BMDJ/Service/AppService.swift
+++ BMDJ/Service/AppService.swift
@@ -22,12 +22,25 @@ final class AppService {
         return versionAndBuild
     }
     var isLoggedIn: Bool
+    var isShowGuide: Bool = false {
+        didSet {
+            if isShowGuide {
+                setGuideHistory()
+            }
+        }
+    }
     
     private init() {
         self.layoutScale = UIScreen.main.bounds.width / 375
         self.isLoggedIn = UserDefaultService.shared.readToken() != nil
-        
-        print(isLoggedIn)
-        
+        self.isShowGuide = fetchGuideHistory()
+    }
+    
+    private func fetchGuideHistory() -> Bool {
+        return UserDefaults.standard.bool(forKey: "AppService.GuideHistory")
+    }
+    
+    private func setGuideHistory() {
+        UserDefaults.standard.set(true, forKey: "AppService.GuideHistory")
     }
 }
