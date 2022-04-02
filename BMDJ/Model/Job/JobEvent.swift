ec20313b44dde4de32f51188a6afca0ade6f84cf
kimjinu <kimjinu5555@gmail.com>
2022-04-02 18:24:45 +0900

    [Add] Job 스키마 추가

---
+++ BMDJ/Model/Job/JobEvent.swift
@@ -0,0 +1,13 @@
+//
+//  JobEvent.swift
+//  BMDJ
+//
+//  Created by 김진우 on 2022/03/06.
+//
+
+enum JobEvent {
+    case delete
+    case add
+    case sort
+    case update
+}
---
+++ BMDJ/Model/Job/JobTarget.swift
@@ -0,0 +1,11 @@
+//
+//  JobTarget.swift
+//  BMDJ
+//
+//  Created by 김진우 on 2022/03/06.
+//
+
+enum JobTarget {
+    case danji
+    case memo
+}
