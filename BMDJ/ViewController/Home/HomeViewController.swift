de44f1d661dff5ca23dacad3a78029cadf35e1d7
kimjinu <kimjinu5555@gmail.com>
2022-04-02 19:02:01 +0900

    [Modify] HomeVC 단지 리스트 변화시 맨 앞으로 스크롤

--- BMDJ/ViewController/Home/HomeViewController.swift
+++ BMDJ/ViewController/Home/HomeViewController.swift
@@ -487,6 +487,15 @@ final class HomeViewController: UIViewController, View {
             .bind(to: nextButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
+        reactor.state.compactMap { $0.scrollToFirst }
+            .bind { _ in
+                DispatchQueue.main.async { [weak self] in
+                    self?.danjiCollectionView.scrollToItem(at: .init(item: 0, section: 0), at: .left, animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        JobService.shared.isNeedCheck = true
         Observable.just(())
             .map { Reactor.Action.refresh }
             .bind(to: reactor.action)
