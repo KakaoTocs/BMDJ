147b469d307846badb85727a34f28363604dae83
kimjinu <kimjinu5555@gmail.com>
2022-07-03 19:05:36 +0900

    [Modify] Memo API 인터페이스 수정 (반환타입 - BDResponse적용)

--- BMDJ/Network/Memo/MemoClient.swift
+++ BMDJ/Network/Memo/MemoClient.swift
@@ -25,7 +25,7 @@ final class MemoClient {
         var result: [Memo]?
         print(MemoRouter.all(danjiID: danjiID).urlRequest?.curlString ?? "nil")
         AF.request(MemoRouter.all(danjiID: danjiID))
-            .responseData { [weak self] response in
+            .response { [weak self] response in
                 if let data = response.data,
                    let memos = try? self?.decoder.decode(NetworkResult<[Memo]>.self, from: data) {
                     print("\(danjiID) Succes")
@@ -38,6 +38,143 @@ final class MemoClient {
             }
         semaphore.wait()
         return result
+    }
+    
+    func allW(danjiID: String) -> BDResponse<[Memo]> {
+        let semaphore = DispatchSemaphore(value: 0)
+        var bdResponse: BDResponse<[Memo]> = .init(data: nil, error: .request)
+        
+        AF.request(MemoRouter.all(danjiID: danjiID))
+            .response { [weak self] response in
+                guard let self = self else {
+                    semaphore.signal()
+                    return
+                }
+                if let response = response.response {
+                    switch response.statusCode {
+                    case 400...499:
+                        bdResponse = .init(data: nil, error: .request)
+                    case 500...599:
+                        bdResponse = .init(data: nil, error: .server)
+                    default:
+                        break
+                    }
+                } else if let data = response.data {
+                    if let memos = try? self.decoder.decode(NetworkResult<[Memo]>.self, from: data).data {
+                        bdResponse = .init(data: memos, error: nil)
+                    } else {
+                        bdResponse = .init(data: nil, error: .scheme)
+                    }
+                } else {
+                    bdResponse = .init(data: nil, error: .response)
+                }
+                semaphore.signal()
+            }
+        semaphore.wait()
+        return bdResponse
+    }
+    
+    func deleteW(id: String) -> BDResponse<String> {
+        let semaphore = DispatchSemaphore(value: 0)
+        var bdResponse: BDResponse<String> = .init(data: nil, error: .request)
+        
+        AF.request(MemoRouter.delete(id: id))
+            .response { response in
+                if let response = response.response {
+                    switch response.statusCode {
+                    case 200:
+                        bdResponse = .init(data: id, error: nil)
+                    case 400...499:
+                        bdResponse = .init(data: nil, error: .request)
+                    case 500...599:
+                        bdResponse = .init(data: nil, error: .server)
+                    default:
+                        break
+                    }
+                } else {
+                    bdResponse = .init(data: nil, error: .response)
+                }
+                semaphore.signal()
+            }
+        semaphore.wait()
+        return bdResponse
+    }
+    
+    func addW(memoCreate: MemoCreate) -> BDResponse<Memo> {
+        let semaphore = DispatchSemaphore(value: 0)
+        var bdResponse: BDResponse<Memo> = .init(data: nil, error: .request)
+        
+        guard let moodData = memoCreate.mood.rawValue.data(using: .utf8),
+              let textData = memoCreate.text.data(using: .utf8),
+              let danjiIDData = memoCreate.danjiId.data(using: .utf8) else {
+            return bdResponse
+        }
+        AF.upload(multipartFormData: { formData in
+            if let imageData = memoCreate.imageData {
+                formData.append(imageData, withName: "image", fileName: "image.jpg", mimeType: "image/jpg")
+            }
+            formData.append(moodData, withName: "mood")
+            formData.append(textData, withName: "text")
+            formData.append(danjiIDData, withName: "danjiId")
+        }, with: MemoRouter.add)
+            .response { [weak self] response in
+                guard let self = self else {
+                    semaphore.signal()
+                    return
+                }
+                if let response = response.response {
+                    switch response.statusCode {
+                    case 400...499:
+                        bdResponse = .init(data: nil, error: .request)
+                    case 500...599:
+                        bdResponse = .init(data: nil, error: .server)
+                    default:
+                        break
+                    }
+                } else if let data = response.data {
+                    if let memo = try? self.decoder.decode(Memo.self, from: data) {
+                       bdResponse = .init(data: memo, error: nil)
+                   } else {
+                       bdResponse = .init(data: nil, error: .scheme)
+                   }
+                } else {
+                    bdResponse = .init(data: nil, error: .response)
+                }
+                semaphore.signal()
+            }
+        semaphore.wait()
+        return bdResponse
+    }
+    
+    func updateW(id: String, text: String) -> BDResponse<String> {
+        let semaphore = DispatchSemaphore(value: 0)
+        var bdResponse: BDResponse<String> = .init(data: nil, error: .request)
+        
+        guard let textData = text.data(using: .utf8) else {
+            return bdResponse
+        }
+        AF.upload(multipartFormData: { formData in
+            formData.append(textData, withName: "text")
+        }, with: MemoRouter.update(id: id))
+            .response { response in
+                if let response = response.response {
+                    switch response.statusCode {
+                    case 200:
+                        bdResponse = .init(data: id, error: nil)
+                    case 400...499:
+                        bdResponse = .init(data: nil, error: .request)
+                    case 500...599:
+                        bdResponse = .init(data: nil, error: .server)
+                    default:
+                        break
+                    }
+                } else {
+                    bdResponse = .init(data: nil, error: .response)
+                }
+                semaphore.signal()
+            }
+        semaphore.wait()
+        return bdResponse
     }
     
     func all(danjiID: String) -> Observable<[Memo]> {
@@ -67,7 +204,7 @@ final class MemoClient {
             if let text = text.data(using: .utf8) {
                 formData.append(text, withName: "text")
             }
-        }, urlRequest: MemoRouter.update(id: id, text: text))
+        }, urlRequest: MemoRouter.update(id: id))
         .map { _ in true }
     }
     func delete(id: String) -> Observable<Bool> {
